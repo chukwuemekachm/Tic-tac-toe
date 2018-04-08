7982054705247f3b434d7cf4624fb4a276f0d459
CHIMA CHUKWUEMEKA EMMANUEL <chukwuemekachm@gmail.com>
2018-04-08 01:48:21 +0100

    After AI play

---
+++ app/src/main/java/com/example/user/tic_tac_toe/AboutAppFragmentDialog.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018 The Android Tic-tac-toe Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.user.tic_tac_toe;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+/*
+*   This class extends the DialogFragment
+*   It is used to set a layout file to the About Dialog
+*/
+public class AboutAppFragmentDialog extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstances) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        //  Get Layout Inflator from current Activity
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        //  Inflate and set the dialog's layout
+        builder.setView(inflater.inflate(R.layout.about_dialog, null)) // Set parent view to null
+
+                //  Add Dialog buttons
+                .setNegativeButton(R.string.close, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        getDialog().cancel();
+                    }
+                });
+        return builder.create();
+    }
+}
---
+++ app/src/main/java/com/example/user/tic_tac_toe/ComputerFiveActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.tic_tac_toe;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ComputerFiveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_computer_five);
+    }
+}
